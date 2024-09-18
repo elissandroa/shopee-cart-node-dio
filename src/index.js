690317fc7d55774d84565ecc469224090c110d32
elissandroa <elissandroa@hotmail.com>
2024-09-18 17:55:45 -0300

    Display Cart

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ const myWishList = [];
 console.log('Welcome to the Shopee Cart ')
 
 const item1 = await createItem("HD 500", 30.25, 8);
-const item2 = await createItem("HD 320", 12.00, 5);
+const item2 = await createItem("HD 320", 12.00, 2);
+const item3 = await createItem("HD 1TB", 100.00, 3);
+const item4 = await createItem("HD 2TB", 250.00, 5);
 
 await cartService.addItem(myCart, item1);
 await cartService.addItem(myCart, item2);
-await cartService.deleteItem(myCart, item1.name)
+await cartService.addItem(myCart, item3);
+await cartService.addItem(myCart, item4);
 
-console.log('Shopee Cart TOTAL IS:')
-console.log("Carrinho:", await cartService.caculateTotal(myCart));
-console.log("Lista de Desejos:", await cartService.caculateTotal(myWishList));
+await cartService.deleteItem(myCart, item1.name)
+await cartService.displayCart(myCart);
+
+console.log("🎁Total:", await cartService.caculateTotal(myCart));
+
 
--- src/services/cartService.js
+++ src/services/cartService.js
@@ -3,11 +3,22 @@
 async function addItem(userCart, item) {
     userCart.push(item);
 }
+
+/* Mostrar Items do carrinho */
+async function displayCart(userCart) {
+    console.log("\nShopee cart list:")
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price.toFixed(2)}  | ${item.quantity}X | Subtotal = ${item.subtotal()}`)
+    })
+}
+
 /* Calcular o total do carrinho */
 async function caculateTotal(userCart) {
+    console.log('\nShopee Cart TOTAL IS:')
     const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
     return result.toFixed(2);
 }
+
 /* Deletar item do carrinho */
 async function deleteItem(userCart, name) { 
     const index = userCart.findIndex((item) => item.name === name);
@@ -26,4 +37,5 @@ export {
     caculateTotal,
     deleteItem, 
     removeItem,
+    displayCart,
 }
