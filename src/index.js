98424afa2fa48a452430ce92d03d351b02d8c056
elissandroa <elissandroa@hotmail.com>
2024-09-18 15:41:26 -0300

    Implementando a lógica do Item

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 const cart = [];
 
+import createItem from "./services/ItemService.js";
 
+console.log('Welcome to the Shopee Cart ')
 
+const item1 = await createItem("HD 500", 30.25, 3);
+const item2 = await createItem("HD 320", 12.00, 1);
+
+cart.push(item1)
+cart.push(item2)
+console.log(item2.subtotal())
--- src/services/ItemService.js
+++ src/services/ItemService.js
@@ -1,5 +1,12 @@
 /* Criar item com subtotal certo */
 
 async function  createItem(name, price, quantity) {
-    
+    return {
+        name,
+        price,
+        quantity,
+        subtotal: () => price * quantity,
+    }
 }
+
+export default createItem;
